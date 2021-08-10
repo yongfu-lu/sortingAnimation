88b45c7c6e1c4ca89e93dd2ddfc31a8609240836
yongfu-lu <yongfu2015@gmail.com>
2021-08-09 21:44:55 -0400

    bubble sort animation added

--- sketch.js
+++ sketch.js
@@ -1,26 +1,55 @@
-let width = window.innerWidth;
-let height = window.innerHeight;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-let nums = []
+var values = []
+var i = 0;
+var j = 0;
 
 function setup() {
-    createCanvas(width,height);
-    fillValue(nums);
+    createCanvas(800,500);
+    fillValue(values);
   }
   
   function draw() {
     background(255);
-    drawValue(nums);
+    bubbleSortByFrame(values);
+    stroke(0);
+    drawValue(values,j);
   }
 
-  function fillValue(nums){
-      for (let i = 0; i < width; i++){
-          nums[i] = floor(random(height));
+  function fillValue(values){
+      for (let i = 0; i < 60; i++){
+          values[i] = floor(random(height));
       }
   }
 
-  function drawValue(nums){
-    for(let w = 0; w <nums.length; w++){
-        line(w,height-nums[w],w,height)
+  function drawValue(values, currentIndex){
+    for(let i = 0; i < values.length; i++){
+        if (i == currentIndex){
+          fill(200,0,200);
+        }
+        else fill(255);
+
+        rect(i*10+100,height-values[i],10,values[i]);
+        //line(i, height, i, height-values[i]);
     }
+  }
+
+  function bubbleSortByFrame(values){
+    if( i < values.length){
+      if(values[j] > values[j+1]){
+        swap(values,j,j+1);
+      }
+      j++;
+      if ( j >= values.length-i-1){
+        j = 0;
+        i ++;
+      }
+    }
+  }
+
+  function swap(arr,a,b){
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
   }
